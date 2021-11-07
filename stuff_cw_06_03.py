3ddf4beb1ce5dcdb01bdfa99877e681e0f826fcf
Guro <demensdeo@gmail.com>
2021-11-08 02:10:33 +0300

    Who_likes_this

--- stuff_cw_06_03.py
+++ stuff_cw_06_03.py
@@ -9,4 +9,21 @@ def who_like_this(names):
         return f'{names[0]}, {names[1]} and {names[2]} like this'
     else:
         return f'{names[0]}, {names[1]} and {len(names) - 2} others like this'
-    
+
+
+print(who_like_this([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
+
+
+def likes(names):
+    length = len(names)
+    return {
+        0: 'no one likes this',
+        1: '{} likes this',
+        2: '{} and {} like this',
+        3: '{}, {} and {} like this',
+        4: '{}, {} and {others} others like this'
+    }[min(4, length)].format(*names[:3], others=length-2)
+
+
+print(likes([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
+
