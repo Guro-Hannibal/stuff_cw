f5620a7f515dcee6b82fd3f5a4aa42b5c9a143e4
Guro <demensdeo@gmail.com>
2021-11-08 01:29:41 +0300

    cw_06

--- stuff_cw_06_02.py
+++ stuff_cw_06_02.py
@@ -12,4 +12,17 @@ def is_prime(num):
     return True
 
 
+def is_prime_1line(num):
+    return num > 1 and all(num % dev for dev in range(2, int(math.sqrt(num)) + 1))
+
+
+def test(num):
+    result = [all(num % dev for dev in range(2, int(math.sqrt(num)) + 1))]
+    return result
+
+
 print(is_prime(73))
+
+print(is_prime_1line(73))
+
+print(test(70))
